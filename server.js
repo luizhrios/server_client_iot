2cc6092954a2ec32778cc3bd841ed646f8491276
Luiz Henrique Brito Rios <lhbrios@tjba.jus.br>
2019-09-27 11:06:47 -0300

    CO 2/3

--- server.js
+++ server.js
@@ -1,61 +1,47 @@
 const net = require('net')
 const fs = require('fs');
+const os = require('os');
 
+let status = {
+  metal: false,
+  vidro: false,
+  plastico: false,
+  papel: false
+};
 let config;
-let devices = [];
-let readyDevices = [];
 
-let devicesFile = fs.readFileSync('devices_list.txt').toString();
 let configFile = fs.readFileSync('config.txt').toString();
 
+let configRegex = /(?<name>\w+) (?<port>\d{2,5})/g;
+let contentRegex = /(?<device>\w+) (?<command>\w+) (?<me>\w+)/g;
+
 parseConfig();
-parseDevicesList();
 
 function parseConfig() {
-  let configRegex = /(?<name>\w+) (?<status>\w+) (?<updateInterval>\d{1,3}) (?<port>\d{2,5})/g;
   config = configRegex.exec(configFile).groups
-}
-
-function parseDevicesList() {
-  let devicesRegex = /(?<name>\w+) (?<host>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) (?<port>\d{2,5})/g;
-  let device;
-  while ((device = devicesRegex.exec(devicesFile)) != null) {
-    devices.push(device.groups)
-  }
+  config.ip = os.networkInterfaces().wlp1s0[0].address;
 }
 
 let server = net.createServer(undefined, (c) => {
-  c.on('data', (data) => handleMessage(data, c))
-  c.on('end', () => readyDevices.splice(readyDevices.findIndex(readyDevice => readyDevice === c)))
-  c.on('error', (err) => handleDisconnect(c, err));
-}).listen(config.port, "0.0.0.0");
+  c.on('data', (data) => handleMessage(data.toString(), c))
+}).listen(config.port, "0.0.0.0", () => {
+  console.log(`Listening on ${config.ip}:${config.port}`)
+});
 
-function handleDisconnect(c, err) {
-  let readyDevice = readyDevices.splice(readyDevices.findIndex(readyDevice => readyDevice === c));
-  console.log(readyDevice.name);
+function handleMessage(message, c) {
+  content = contentRegex.exec(message);
+  if (content) {
+    content = content.groups
+    handleCommand(content, c);
+  }
 }
 
-function handleMessage(data, c) {
-  if (config.status == 'ATIVADO') {
-    if (!readyDevices.find(readyDevice => readyDevice === c)) {
-      let content = data.toString();
-      let contentRegex = /(?<device>\w+) (?<command>\w+) (?<me>\w+)/g;
-      content = contentRegex.exec(content).groups;
-      console.log(`${content.device} connected`)
-      c.write(`${config.name} ${config.status} ${config.updateInterval}`)
-      c.name = content.device;
-      readyDevices.push(c)
-    }
-  }
-  else {
-    c.end('DESATIVADO')
-  }
+function handleCommand(command, c) {
+  console.log(`${content.device} connected`)
+  c.write(`${config.name} ${''}`)
+  c.name = content.device;
 }
 
 setInterval(() => {
-  readyDevices.forEach(readyDevice => {
-    readyDevice.write((Math.random() * 100).toString()+'\n')
-  })
+  
 }, config.updateInterval * 1000)
-
-module.exports = { me: config.name, devices }
