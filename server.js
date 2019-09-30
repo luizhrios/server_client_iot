853ecd1375696f47ec6e82398cc8b038c84db49b
Luiz Henrique Brito Rios <lhbrios@tjba.jus.br>
2019-09-30 15:55:34 -0300

    final

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const net = require('net')
 const fs = require('fs');
 const os = require('os');
+// const RC4Class = require('rc4-ts').default;
+
+// const encryptKey = 'serveriot';
+// const rc4 = new RC4Class(encryptKey);
 
 let status = {
   metal: false,
@@ -19,14 +23,27 @@ parseConfig();
 
 function parseConfig() {
   config = configRegex.exec(configFile).groups
-  config.ip = os.networkInterfaces().wlp1s0[0].address;
+  config.ip = os.networkInterfaces().enp0s31f6[0].address;
 }
 
 let server = net.createServer(undefined, (c) => {
-  c.on('data', (data) => handleMessage(data.toString(), c))
+  c.on('data', data => handleData(data, c))
+  c.on('error', err => console.log(err))
 }).listen(config.port, "0.0.0.0", () => {
   console.log(`Listening on ${config.ip}:${config.port}`)
 });
+
+function handleData(data, c) {
+  // try {
+  //   const decryptResult = rc4.decrypt(message)
+  //   decryptResult = new Buffer(decryptResult, 'base64').toString();
+  //   message = decryptResult;
+  // } catch (error) {
+
+  // }
+  message = data.toString();
+  handleMessage(message, c);
+}
 
 function handleMessage(message, c) {
   content = contentRegex.exec(message);
@@ -39,7 +56,7 @@ function handleMessage(message, c) {
 function handleCommand(content, c) {
   console.log(`${content.device} connected: ${content.command}`)
   if (content.command === 'STATUS')
-    c.end(`${config.name} ${status.metal ? 1 : 0} ${status.vidro ? 1 : 0} ${status.plastico ? 1 : 0} ${status.papel ? 1 : 0}\n`)
+    c.end(`${config.name} ${status.metal ? 1 : 0}${status.vidro ? 1 : 0}${status.plastico ? 1 : 0}${status.papel ? 1 : 0}\n`)
   else if (content.command === 'CARREGAR') {
     let n = 0;
     for (const material in status) {
